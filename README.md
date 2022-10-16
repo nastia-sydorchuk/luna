35bf836f11da1dc00d680ce25c0b5e92041c4be2
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-10-16 22:21:26 +0100

    effects were added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Luna landing
+  Landing page for Luna – a pixel-perfect landing page with responsive design techniques, so the website could be viewed on any device.
+  Technologies: HTML, CSS, Sass (SCSS), BEM, JavaScript
+
+  - [DEMO LINK](https://nastia-sydorchuk.github.io/luna/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Slider
 const features = [...document.querySelectorAll('.feature')];
 const prev = document.querySelector('.icon--backward');
 const next = document.querySelector('.icon--forward');
@@ -24,6 +25,7 @@ const move = (currentPosition) => {
 prev.addEventListener('click', () => move(position - 1));
 next.addEventListener('click', () => move(position + 1));
 
+// Listening to Scroll
 function isInView(elem) {
   const elemTop = elem.getBoundingClientRect().top;
   const elemBottom = elem.getBoundingClientRect().bottom;
@@ -33,12 +35,100 @@ function isInView(elem) {
 
 const videoElem = document.querySelector('.presentation__video');
 let videoWasStarted = false;
+const nav = document.querySelector('nav');
+const cross = document.querySelector('.icon--cross');
+const menu = document.querySelector('.icon--menu');
 
 window.addEventListener('scroll', () => {
+  const navTop = nav.getBoundingClientRect().top;
+
+  if (navTop < 0) {
+    nav.style.transform = 'translateY(-100%)';
+    window.history.pushState({}, 'Home', document.location.origin);
+  }
+
   if (isInView(videoElem) && !videoWasStarted) {
     videoWasStarted = true;
     videoElem.play();
   } else if (!isInView(videoElem)) {
     videoElem.pause();
   }
+});
+
+menu.addEventListener('click', () => {
+  nav.style.transform = 'translateY(0)';
+});
+
+cross.addEventListener('click', () => {
+  nav.style.transform = 'translateY(-100%)';
+});
+
+// Form validation
+
+const emailInput = document.querySelector('.form__field--email');
+const messageInput = document.querySelector('.form__field--textarea');
+const successMessage = document.querySelector('.form__message--success');
+const form = document.querySelector('form');
+
+const inputs = [emailInput, messageInput];
+let formIsValid = false;
+let isValidating = false;
+
+const isEmailValid = (email) => {
+  // eslint-disable-next-line
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return regex.test(email.toLowerCase());
+};
+
+const resetInput = (element) => {
+  element.previousElementSibling.classList.add('form__message--hidden');
+  element.classList.remove('form__field--invalid');
+};
+
+const invalidateInput = (element) => {
+  element.previousElementSibling.classList.remove('form__message--hidden');
+  element.classList.add('form__field--invalid');
+};
+
+const validateInputs = () => {
+  if (!isValidating) {
+    return;
+  }
+
+  inputs.forEach(resetInput);
+  formIsValid = true;
+
+  if (!messageInput.value) {
+    invalidateInput(messageInput);
+    formIsValid = false;
+  }
+
+  if (!isEmailValid(emailInput.value)) {
+    invalidateInput(emailInput);
+    formIsValid = false;
+  }
+};
+
+const clearForm = () => {
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  isValidating = true;
+  validateInputs();
+
+  if (formIsValid) {
+    clearForm();
+    successMessage.classList.remove('form__message--hidden');
+  }
+});
+
+inputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    resetInput(input);
+    successMessage.classList.add('form__message--hidden');
+  });
 });
